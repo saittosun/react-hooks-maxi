63ba0c33cd2de0f3c8e82c572b93301474b75270
tostos <saittosun2@gmail.com>
2020-04-28 19:28:27 +0200

    passing state data across components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -19,7 +19,7 @@ const IngredientForm = React.memo(props => {
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount});
   };
 
   return (
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,30 @@
-import React from 'react';
+// jshint esversion: 9
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients(prevIngredients => [
+      //  here the idea will be to call set user ingredients and now important we'll need to update the existing list of ingredients and add a new one. Now that means we depend on the current state and they offer it's best if we use the functional forum where we're guaranteed to get the latest state. So we get our previous ingredients here our older array basically the current state. This is a spread operator taking all elements of our old array and adding them as elements to this new Array which I'm constructing with the square brackets. And then to add one new element here and that's the ingredient we're getting now important ingredient we're getting from the ingredient form we'll have a title and an amount.
+      ...prevIngredients, 
+      {id: Math.random().toString(),
+        // we can again use to spread operator on ingredient now. So on this argument we're getting because this argument will be an object let's say the spread operator then takes all key value pairs from that object and adds key and value pairs to this new object. So now we're adding a new object with an I.D. title in amount to our list of ingredients here.
+        ...ingredient}
+    ]);
+  };
   return (
     <div className="App">
-      <IngredientForm />
-
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList 
+          ingredients={userIngredients}
+          onRemoveItem={() => {}} />
       </section>
     </div>
   );
