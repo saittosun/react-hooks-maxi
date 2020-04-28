aec11bd0275187568f91da162397d5f6d46030f7
tostos <saittosun2@gmail.com>
2020-04-28 19:44:09 +0200

    removing ingredient-assignment1

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,6 +8,14 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients(prevIngredients => {
+      return prevIngredients.filter((ingredient) => {
+        return ingredient.id !== ingredientId;
+      });
+    });
+  };
+
   const addIngredientHandler = (ingredient) => {
     setUserIngredients(prevIngredients => [
       //  here the idea will be to call set user ingredients and now important we'll need to update the existing list of ingredients and add a new one. Now that means we depend on the current state and they offer it's best if we use the functional forum where we're guaranteed to get the latest state. So we get our previous ingredients here our older array basically the current state. This is a spread operator taking all elements of our old array and adding them as elements to this new Array which I'm constructing with the square brackets. And then to add one new element here and that's the ingredient we're getting now important ingredient we're getting from the ingredient form we'll have a title and an amount.
@@ -24,7 +32,7 @@ const Ingredients = () => {
         <Search />
         <IngredientList 
           ingredients={userIngredients}
-          onRemoveItem={() => {}} />
+          onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
