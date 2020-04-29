c68f19fb78e72e18c40a586109d37d05b86831fb
tostos <saittosun2@gmail.com>
2020-04-29 11:41:21 +0200

    useCallback

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,5 +1,5 @@
 // jshint esversion: 9
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,31 +7,11 @@ import IngredientList from './IngredientList';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
-
-  // Use effect has to name because it is there for you to manage side effects in HTTP requests are a typical side effect. side effect basically means that you have some logic that runs that does affect your application for example we are fetching some data here but it's not getting finished in this current renderer cycle or maybe it affects something which is outside of the scope of your jsx code down there maybe you're setting the document title anything like that. So anything you can not manage with your normal this component is getting rendered flow use effect and that's important by default gets executed right after important after every component render a cycle. So after this component has been rendered to first time. function you pass to use effect because you have to parse a function there will get executed. So this runs when ever this component got re rendered. And that's really important to keep in mind after and for every render cycle these are two important pieces not before not simultaneously but after and for every render cycle.
-  // Actually all it takes a second argument. The first argument is this function which it executes after every render cycle. The second argument is an array with the dependencies of your function and only when such a dependency changed only then the function will rerun. So this allows you to control how often does function runs by default for every render cycle.
-  useEffect(() => {
-    fetch('https://react-hooks-a1e9b.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-      // this useEffect function here actually has no external dependencies(with external dependencies I mean variables or data you're using which you define in your component outside of the use affect function.) and so we can add an empty array but we have to do that admitting it is not an option.
-      // used like this (with [] as a second argument), useEffect() acts like componentDidMount: it runs ONLY ONCE (after the first render)
-  }, []);
-
+  
   // I'm not specifying a second argument and therefore this will run for every well rerun or a cycle here.
   // If we add second argument, this means that this function here will now only run when user ingredients changed
   useEffect(() => {
-    console.log('rendering ingredients', userIngredients)
+    console.log('rendering ingredients', userIngredients);
   }, [userIngredients]);
 
   const removeIngredientHandler = (ingredientId) => {
@@ -42,9 +22,10 @@ const Ingredients = () => {
     });
   };
 
-  const filteredIngredientsHandler = (filteredIngredients) => {
+  // this will now never rerun and therefore what React does is it cashes your function for you so that it survives rerun or cycles and therefore when ingredients component re renders this specific function (useCallback()) is not re created so it will not change. So what we pass to the search component to onLoadIngredients will be the old function the same function as on the previous render cycle and therefore in the search component on the use effect here on this check onLoadIngredients will not have changed and therefore it is a fact will not rerun.
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
-  }
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     // this is where we want to send our data to and you got to know what have fetched by default will send a get request a firebase once a post request though if we want to store data hence we pass a second argument to fetch and that's an object that allows us to configure this request in on this object we can set the method property to post default the set then you never need to set that but we want to set this to post you then also can add a body property to define what you want to send and that has to be json data which means you can use json which is another thing built into the browser it's a class in the end built in to browsers which has a stringify method. This will take a javascript object or array and convert it into valid json format
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -9,9 +9,11 @@ const Search = React.memo(props => {
   const {onLoadIngredients} = props;
   const [enteredFilter, setEnteredFilter] = useState('');
 
+    // Use effect has to name because it is there for you to manage side effects in HTTP requests are a typical side effect. side effect basically means that you have some logic that runs that does affect your application for example we are fetching some data here but it's not getting finished in this current renderer cycle or maybe it affects something which is outside of the scope of your jsx code down there maybe you're setting the document title anything like that. So anything you can not manage with your normal this component is getting rendered flow use effect and that's important by default gets executed right after important after every component render a cycle. So after this component has been rendered to first time. function you pass to use effect because you have to parse a function there will get executed. So this runs when ever this component got re rendered. And that's really important to keep in mind after and for every render cycle these are two important pieces not before not simultaneously but after and for every render cycle.
+  // Actually all it takes a second argument. The first argument is this function which it executes after every render cycle. The second argument is an array with the dependencies of your function and only when such a dependency changed only then the function will rerun. So this allows you to control how often does function runs by default for every render cycle.
   useEffect(() => {
     // we can use string interpolation to dynamically inject values
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo"${enteredFilter}"`;
+    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
     fetch('https://react-hooks-a1e9b.firebaseio.com/ingredients.json' + query)
       // sonra firebase de rules bolumunde write in altina bunu ilave ettik => "ingredients": {".indexOn": ["title"]}.  with the setup we can filter for the title.
       .then(response => response.json())
@@ -25,8 +27,10 @@ const Search = React.memo(props => {
           });
         }
         // I want to trigger something in ingredients.js. Right because that is where we manage our ingredients. That is where we also use the search component. So in the end we should establish a connection between search and this ingredients component to update the ingredients here whenever we fetched new ingredients in search.js.
-        // onLoadIngredients(loadedIngredients);
+        onLoadIngredients(loadedIngredients);
       });
+      // this useEffect function here actually has no external dependencies(with external dependencies I mean variables or data you're using which you define in your component outside of the use affect function.) and so we can add an empty array but we have to do that admitting it is not an option.
+      // used like this (with [] as a second argument), useEffect() acts like componentDidMount: it runs ONLY ONCE (after the first render)
   }, [enteredFilter, onLoadIngredients]);
 
   return (
