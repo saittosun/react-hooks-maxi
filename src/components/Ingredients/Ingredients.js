f9b1b7dacc2b58f59930e41a903c1c8750efb8ab
tostos <saittosun2@gmail.com>
2020-04-29 05:12:19 +0200

    useEffect() & loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,5 +1,5 @@
 // jshint esversion: 9
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,6 +7,26 @@ import IngredientList from './IngredientList';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  // Use effect has to name because it is there for you to manage side effects in HTTP requests are a typical side effect. side effect basically means that you have some logic that runs that does affect your application for example we are fetching some data here but it's not getting finished in this current renderer cycle or maybe it affects something which is outside of the scope of your jsx code down there maybe you're setting the document title anything like that. So anything you can not manage with your normal this component is getting rendered flow use effect and that's important by default gets executed right after important after every component render a cycle. So after this component has been rendered to first time. function you pass to use effect because you have to parse a function there will get executed. So this runs when ever this component got re rendered. And that's really important to keep in mind after and for every render cycle these are two important pieces not before not simultaneously but after and for every render cycle.
+  // Actually all it takes a second argument. The first argument is this function which it executes after every render cycle. The second argument is an array with the dependencies of your function and only when such a dependency changed only then the function will rerun. So this allows you to control how often does function runs by default for every render cycle.
+  useEffect(() => {
+    fetch('https://react-hooks-a1e9b.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+      // this useEffect function here actually has no external dependencies(with external dependencies I mean variables or data you're using which you define in your component outside of the use affect function.) and so we can add an empty array but we have to do that admitting it is not an option.
+      // used like this (with [] as a second argument), useEffect() acts like componentDidMount: it runs ONLY ONCE (after the first render)
+  }, []);
 
   const removeIngredientHandler = (ingredientId) => {
     setUserIngredients(prevIngredients => {
